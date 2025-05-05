62f78b902ff6023e8b8be4be11e2987adeaec787
Aarzoomali <156358713+Aarzoomali@users.noreply.github.com>
2025-05-05 20:36:32 +0000

    added requirements.txt

--- elt/basket_craft_website_sessions_extract_load_raw.py
+++ elt/basket_craft_website_sessions_extract_load_raw.py
@@ -1,16 +1,22 @@
 import os
 import pandas as pd
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+# load env vars from .env file
+load_dotenv()
 
 # ✅ Load secrets from GitHub Actions environment
-mysql_user = "analyst"
-mysql_password = "go_lions"
-mysql_host = "db.isba.co"
-mysql_db = "basket_craft"
-pg_user = "analyst"
-pg_password = "go_lions"
-pg_host = "db.isba.co"
-pg_db = "basket_craft"
+
+mysql_user = os.getenv("MYSQL_USER")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_host = os.getenv("MYSQL_HOST")
+mysql_db = os.getenv("MYSQL_DB")
+
+pg_user = os.getenv("PG_USER")
+pg_password = os.getenv("PG_PASSWORD")
+pg_host = os.getenv("PG_HOST")
+pg_db = os.getenv("PG_DB")
 
 print("✅ Secrets loaded:")
 print("MYSQL_USER:", mysql_user)
@@ -31,6 +37,7 @@ WHERE created_at BETWEEN '2023-12-01' AND '2023-12-31 23:59:59';
 """
 df = pd.read_sql(query, mysql_engine)
 
+
 # ✅ Connect to Postgres (destination)
 pg_url = f"postgresql+psycopg2://{pg_user}:{pg_password}@{pg_host}/{pg_db}"
 pg_engine = create_engine(pg_url)
@@ -39,3 +46,4 @@ pg_engine = create_engine(pg_url)
 df.to_sql('website_sessions', pg_engine, schema='raw', if_exists='replace', index=False)
 
 print("✅ Data successfully loaded into raw.website_sessions")
+
